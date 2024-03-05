536d8f26eeafe0e4aa90ecdc545fed325ae67f94
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-03-05 21:57:22 +0700

    working root finder

--- CubicSolver/Program.cs
+++ CubicSolver/Program.cs
@@ -4,6 +4,7 @@ using MathNet.Numerics.RootFinding;
 using System.Text.RegularExpressions;
 using System.Numerics;
 using MathNet.Symbolics;
+using MathNet.Numerics;
 
 
 double result = SimpsonRule.IntegrateComposite(x => x * x, 0.0, 10.0, 4);
@@ -18,25 +19,37 @@ double c = 11;
 double d = -6;
 
 var roots = Cubic.RealRoots(d, c, b);
+Console.WriteLine("\nusing Cubic.RealRoots :" + roots); // 3,2,1
+
 (Complex root1, Complex root2, Complex root3) = Cubic.Roots(d, c, b, a);
+Console.WriteLine("\nusing complex root : " + root1.Real + " " + root2.Real + " " + root3.Real); // 1,3,2
+
 //var roots = System.Numerics.Complex.
 //Func<double, double> f = x => Math.Exp(-x) - x;
 //Func<double, double> f = x => Math.Pow(x, 3) + b * Math.Pow(x, 2) + c * x + d;
 //Func<double, double> f = x => x * Math.Pow(x, 3) + Math.Pow(x, 2) - 4 * x - 4;
 //Func<double, double> f = x => x* Math.Pow(x, 3) - (6 * Math.Pow(x, 2)) + 11 * x + (-6);
+
 Func<double, double> f = x => a * Math.Pow(x, 3) + b * Math.Pow(x, 2) + c * x + d;
 // define the derivative of the cubic function
 Func<double, double> df = x => 3 * a * Math.Pow(x, 2) + 2 * b * x + c;
 double lowerBound = -6;
-double upperBound = 5;
+double upperBound = 11;
 double accuracy = 1e-6;
 int maxIterations = 100;
 double root = Bisection.FindRoot(f, lowerBound, upperBound, accuracy, maxIterations);
+Console.WriteLine("\nBisection.FindRoot : " + root.Round(1)); // 3
+
 double expandFactor = 1.6;
 double expandRoot = Bisection.FindRootExpand(f, lowerBound, upperBound, accuracy, maxIterations, expandFactor);
-double mRoot = NewtonRaphson.FindRoot(f, df, lowerBound, upperBound, accuracy, maxIterations);
-//(double root1, double root2, double root3) = Cubic.RealRoots(d, c, b);
-//(MathNet.Numerics.LinearAlgebra.Complex root1, Complex root2, Complex root3) = MathNet.Numerics.RootFinding.Bisection(d, c, b);
+Console.WriteLine("\nBisection.FindRootExpand : " + expandRoot.Round(1)); // 3
+
+double mTryRoot;
+bool tryRoot = Bisection.TryFindRoot(f, lowerBound, upperBound, accuracy, maxIterations, out mTryRoot);
+Console.WriteLine("\nTryFindRoot : " + mTryRoot.Round(1)); // 3
+
+double mNewton = NewtonRaphson.FindRoot(f, df, lowerBound, upperBound, accuracy, maxIterations);
+Console.WriteLine("\nNewtonRaphson : " + mNewton.Round(1)); // 1
 
 string equation = "f(x) = ax^3 + bx^2 + cx + d";
 ParseEquation(equation);
@@ -53,19 +66,19 @@ static void ParseEquation(string equation)
         string c = match.Groups["c"].Value;
         string d = match.Groups["d"].Value;
 
-        Console.WriteLine($"a: { a}, b: { b}, c: { c}, d: { d}");
+        Console.WriteLine($"\na: { a}, b: { b}, c: { c}, d: { d}");
 
         // Now you can use these coefficients with MathNet as needed
         // For example, to create a symbolic expression:
         var x = SymbolicExpression.Variable("x");
         //var cubicExpression = SymbolicExpression.Parse($"{a} *x ^ 3 + {b}" + $" *x ^ 2 + {c} *x + {d}");
         var cubicExpression = a  + "x^3" + " + " + b + "x^2" + " + " + c + "*x" + " + " + d;
-        Console.WriteLine($"Expression: {cubicExpression}");
+        Console.WriteLine($"\nExpression: {cubicExpression}");
     }
     else
     {
-        Console.WriteLine("The equation format is incorrect.");
+        Console.WriteLine("\nThe equation format is incorrect.");
     }
 }
 
-Console.WriteLine("The Integral of x^2 from 0 to 10 is : " + result);
+Console.WriteLine("\nThe Integral of x^2 from 0 to 10 is : " + result);
