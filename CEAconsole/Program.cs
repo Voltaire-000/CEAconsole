6cebef6e1167221145554a2fb70672a93c67a581
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-03-06 20:43:04 +0700

    in process

--- CEAconsole/Program.cs
+++ CEAconsole/Program.cs
@@ -19,14 +19,14 @@ string json = JsonConvert.SerializeObject(reactants, Formatting.Indented);
 
 JArray parsedJson = JArray.Parse(json);
 // filter the object based on the conditions
-JArray filteredReactants = new JArray(parsedJson.Where(r => r["Name"].ToString() == "CH4"));
+JArray filteredReactants = new(parsedJson.Where(r => r["Name"].ToString() == "CH4"));
 // convert back to a JSON string
-string filteredJson = JsonConvert.SerializeObject(filteredReactants, Formatting.Indented);
+string propertiesOfInputFilter = JsonConvert.SerializeObject(filteredReactants, Formatting.Indented);
 
-JArray mParsedJson = JArray.Parse(filteredJson);
-string name = mParsedJson[0].SelectToken("Name").ToString();
+JArray reactantProperties = JArray.Parse(propertiesOfInputFilter);
+string name = reactantProperties[0].SelectToken("Name").ToString();
 
-JObject chemicalFormula = (JObject)mParsedJson[0].SelectToken("ChemicalFormula");
+JObject chemicalFormula = (JObject)reactantProperties[0].SelectToken("ChemicalFormula");
 foreach (var element in chemicalFormula)
 {
     string elementName = element.Key;
@@ -34,8 +34,20 @@ foreach (var element in chemicalFormula)
     Console.WriteLine($"Element: {elementName}, Value: {elementValue}");
 }
 
+// get number of T_intervals
+string mT_Intervals = reactantProperties[0].SelectToken("T_intervals").ToString();
+dynamic parsedT_Intervals = JsonConvert.DeserializeObject(mT_Intervals);
+int mNumberOfTempIntervals = (int)parsedT_Intervals;
+
+// get temperature range_1
+List<JToken> temperatureRange = reactantProperties[0].SelectToken("TemperatureRange").ToList();
+List<JToken> tempRange_1 = temperatureRange[0].ToList();
+List<JToken> mChildren = tempRange_1.Children().ToList();
+var childTempRange = mChildren[0].ToList();
+var childNumberOfCoefficients = mChildren[1].ToList();
+
 //var filterResult = reactants.Where(d => d.Name == "CH4");
-var mtest = reactants.Where(d => d.Name == "CH4");
+//var mtest = reactants.Where(d => d.Name == "CH4");
 //List<Element>? elements = new(elementsService.GetElements());
 //string json = JsonConvert.SerializeObject(elements, Formatting.Indented);
 
@@ -43,13 +55,19 @@ var mtest = reactants.Where(d => d.Name == "CH4");
 //List<TransportProperty>? transportProperties = new(transportService.GetTransportProperties());
 //string json = JsonConvert.SerializeObject(transportProperties, Formatting.Indented);
 
-Console.WriteLine(filteredJson);
-Console.WriteLine(BalancedEquation(1, 4)); // CH4 + 2O2 -> CO2 + 2H2O
-Combustion combustion = new Combustion(1, 4);
-combustion.BalanceEquation();
-Console.WriteLine(BalancedEquation(2, 6)); // C2H6 + 7/202 -> 2CO2 + 3H2O
-Console.WriteLine(BalancedEquation(3, 8)); // C3H8 + 5O2 -> 3CO2 + 4H2O
-Console.WriteLine(BalancedEquation(4, 10)); // C4H10 + 13/2O2 -> 4CO2 + 5H2O
+
+
+
+Console.WriteLine(propertiesOfInputFilter);
+
+
+
+//Console.WriteLine(BalancedEquation(1, 4)); // CH4 + 2O2 -> CO2 + 2H2O
+//Combustion combustion = new(1, 4);
+//combustion.BalanceEquation();
+//Console.WriteLine(BalancedEquation(2, 6)); // C2H6 + 7/202 -> 2CO2 + 3H2O
+//Console.WriteLine(BalancedEquation(3, 8)); // C3H8 + 5O2 -> 3CO2 + 4H2O
+//Console.WriteLine(BalancedEquation(4, 10)); // C4H10 + 13/2O2 -> 4CO2 + 5H2O
 
 //Console.WriteLine(json);
 static string BalancedEquation(int X, int Y)
