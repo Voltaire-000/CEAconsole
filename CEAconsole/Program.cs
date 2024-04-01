f8a7a246fbf81327a5cfe8f87180645d70c799c7
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-04-01 14:10:03 +0700

    in process

--- CEAconsole/Program.cs
+++ CEAconsole/Program.cs
@@ -55,27 +55,27 @@ if (hasKeyRange_1)
     }
 }
 // list of temperature exponents
-double texp_1 = temperatureExponents[0];
-double texp_2 = temperatureExponents[1];
-double texp_3 = temperatureExponents[2];
-double texp_4 = temperatureExponents[3];
-double texp_5 = temperatureExponents[4];
-double texp_6 = temperatureExponents[5];
-double texp_7 = temperatureExponents[6];
-double texp_8 = temperatureExponents[7];
+//double texp_1 = temperatureExponents[0];
+//double texp_2 = temperatureExponents[1];
+//double texp_3 = temperatureExponents[2];
+//double texp_4 = temperatureExponents[3];
+//double texp_5 = temperatureExponents[4];
+//double texp_6 = temperatureExponents[5];
+//double texp_7 = temperatureExponents[6];
+//double texp_8 = temperatureExponents[7];
 
-// list of coefficients
-double a1 = coefficients[0];
-double a2 = coefficients[1];
-double a3 = coefficients[2];
-double a4 = coefficients[3];
-double a5 = coefficients[4];
-double a6 = coefficients[5];
-double a7 = coefficients[6];
+//// list of coefficients
+//double a1 = coefficients[0];
+//double a2 = coefficients[1];
+//double a3 = coefficients[2];
+//double a4 = coefficients[3];
+//double a5 = coefficients[4];
+//double a6 = coefficients[5];
+//double a7 = coefficients[6];
 
-// list of integration constants
-double a8 = integrationConstants[0];
-double a9 = integrationConstants[1];
+//// list of integration constants
+//double a8 = integrationConstants[0];
+//double a9 = integrationConstants[1];
 
 List<double> temperatureList = [];
 List<double> heatCapacityList = [];
